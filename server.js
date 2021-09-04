b8acb3f01cf751e06cd1c8151ba88de75d67ba35
jsguerra <jose@gcostudios.com>
2021-09-04 10:06:32 +0100

    basic server setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+// Middleware to parse body into json
+// ========================================
+app.use(express.json())
+app.use(express.urlencoded({
+  extended: true
+}))
+
+// Use public directory for root
+// ========================================
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+// Server running and listening
+// ========================================
+app.listen(PORT, () => console.log(`Listening on https://localhost:${PORT}`))
