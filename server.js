287840bc9d6ad56f797cfcec41dd5ba3e5afafea
jsguerra <jose@gcostudios.com>
2021-09-04 22:13:18 +0100

    updated routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const PORT = 3000
+const routes = require('./routes/books')
 
 // Middleware to parse body into json
 // ========================================
@@ -13,6 +14,10 @@ app.use(express.urlencoded({
 // Use public directory for root
 // ========================================
 app.use('/', express.static(path.join(__dirname, 'public')))
+
+// API get all books
+// ========================================
+app.use('/', routes)
 
 // Server running and listening
 // ========================================
