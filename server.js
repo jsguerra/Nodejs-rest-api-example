e9cb9c5867f011ca752a2f44deeafabf474516e9
jsguerra <jose@gcostudios.com>
2021-09-07 12:25:19 +0100

    added cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 const routes = require('./routes/books')
+const cors = require('cors')
 
 // Middleware to parse body into json
 // ========================================
@@ -10,12 +11,13 @@ app.use(express.json())
 app.use(express.urlencoded({
   extended: true
 }))
+app.use(cors())
 
 // Use public directory for root
 // ========================================
 app.use('/', express.static(path.join(__dirname, 'public')))
 
-// API get all books
+// API Routes
 // ========================================
 app.use('/', routes)
 
