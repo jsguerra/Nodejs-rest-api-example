7062a28d6ba2349a10a381d0af0c813d3a50e644
jsguerra <jose@gcostudios.com>
2021-09-14 09:04:28 +0100

    updated model path

--- routes/books.js
+++ routes/books.js
@@ -1,25 +1,25 @@
 const express = require('express')
 const router = express.Router()
-const booksController = require('../controllers/books')
+const model = require('../models/books')
 
 // Create a new book
 // ========================================
-router.post('/api/v1/books', booksController.createBook)
+router.post('/api/v1/books', model.createBook)
 
 // Get all books
 // ========================================
-router.get('/api/v1/books', booksController.getAllBooks)
+router.get('/api/v1/books', model.getAllBooks)
 
 // Get a book
 // ========================================
-router.get('/api/v1/books/:id', booksController.getBook)
+router.get('/api/v1/books/:id', model.getBook)
 
 // Update a book
 // ========================================
-router.patch('/api/v1/books/:id', booksController.updateBook)
+router.patch('/api/v1/books/:id', model.updateBook)
 
 // Delete a book
 // ========================================
-router.delete('/api/v1/books/:id', booksController.deleteBook)
+router.delete('/api/v1/books/:id', model.deleteBook)
 
 module.exports = router
